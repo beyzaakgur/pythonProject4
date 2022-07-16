1d0ebf139f4e106cde153a87273fb66226f75baf
Beyza Akgür <beyzaakgur@gmail.conm>
2022-07-16 20:38:53 +0300

    yebi

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')
 
-
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
